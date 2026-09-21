ea5b408fcbe76182151947ecbbb1f572ceda4e4f
AICells <1713699517@qq.com>
2026-09-21 23:43:25 +0800

    Add HTTP/2 integration test handler

---
+++ test/wsHttp2TestHandler.erl
@@ -0,0 +1,17 @@
+-module(wsHttp2TestHandler).
+
+-include("eWSrv.hrl").
+
+-export([handle/3]).
+
+handle('GET', <<"/one">>, _Req) ->
+   {ok, [{<<"content-type">>, <<"text/plain">>}], <<"one">>};
+handle('GET', <<"/two">>, _Req) ->
+   {ok, [{<<"content-type">>, <<"text/plain">>}], <<"two">>};
+handle('GET', <<"/large">>, _Req) ->
+   {ok, [{<<"content-type">>, <<"application/octet-stream">>}],
+      binary:copy(<<"x">>, 100000)};
+handle('POST', <<"/echo">>, #wsReq{body = Body}) ->
+   {ok, [{<<"content-type">>, <<"application/octet-stream">>}], Body};
+handle(_, _, _) ->
+   {404, [], <<"Not Found">>}.
