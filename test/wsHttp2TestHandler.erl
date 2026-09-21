f9a32c894dceec8758d77ea0a8079870f5ec99dd
AICells <1713699517@qq.com>
2026-09-21 23:51:14 +0800

    Add slow handler for HTTP/2 multiplex test

--- test/wsHttp2TestHandler.erl
+++ test/wsHttp2TestHandler.erl
@@ -8,6 +8,9 @@ handle('GET', <<"/one">>, _Req) ->
    {ok, [{<<"content-type">>, <<"text/plain">>}], <<"one">>};
 handle('GET', <<"/two">>, _Req) ->
    {ok, [{<<"content-type">>, <<"text/plain">>}], <<"two">>};
+handle('GET', <<"/slow">>, _Req) ->
+   timer:sleep(500),
+   {ok, [{<<"content-type">>, <<"text/plain">>}], <<"slow">>};
 handle('GET', <<"/large">>, _Req) ->
    {ok, [{<<"content-type">>, <<"application/octet-stream">>}],
       binary:copy(<<"x">>, 100000)};
